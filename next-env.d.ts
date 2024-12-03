923611153dac36b54b11cd31167311431a8bca43
invisible <robin_04@icloud.com>
2024-12-03 19:16:52 +0100

    a

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,9 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
+declare module 'clsx';
+declare module 'tailwind-merge';
+
 declare namespace React {
     type ReactNode = 
         | string
